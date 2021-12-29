21c612da04a398eea8b4b38e5db2edf1abd987b0
Niklas Moss <nin@envidan.dk>
2021-12-29 14:47:59 +0000

    Configure GitHub CLI to use SSH pr default

--- home/debian-install.sh
+++ home/debian-install.sh
@@ -38,6 +38,7 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
 sudo apt install gh
+gh config set -h github.com git_protocol ssh
 
 # Login to GitHub CLI using information from lastpass  
 lpass show --sync=now "Github PAT" | grep Notes | sed s/Notes:\ // | gh auth login --with-token
