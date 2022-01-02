f8a796b9f3de85cd3b4ca0f47e4e8092f1f7607c
Niklas Moss <nin@envidan.dk>
2022-01-02 12:48:37 +0000

    Track coc-settings, simplify debian-install and fix Brandheroes.yml

--- home/debian-install.sh
+++ home/debian-install.sh
@@ -29,31 +29,51 @@ nvm install 16.13.1
 # Expo
 npm install -g expo
 
+# Yarn
+npm install -g yarn
+
 # LastPass CLI
-sudo apt install -y lastpass-cli
+apt install -y lastpass-cli
 lpass login --trust niklas.noerregaard@gmail.com
 
 # GitHub CLI
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh
+apt update
+apt install -y gh
 gh config set -h github.com git_protocol ssh
 
 # Login to GitHub CLI using information from lastpass  
-lpass show --sync=now "Github PAT" | grep Notes | sed s/Notes:\ // | gh auth login --with-token
+lpass show -c --notes --sync=now "Github PAT" | gh auth login --with-token
 
-# Generate SSH key
+# Generate SSH key and add it to GitHub
 ssh-keygen -t ed25519 -C "Ubuntu PC"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
-
 gh ssh-key add ~/.ssh/id_ed25519.pub
+
+# Install Docker
+apt install ca-certificates gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update # Update with the newly added Docker repository
+apt install docker-ce docker-ce-cli containerd.io
+
+# Ag (the silver searcher) for searching files in Neovim with Denite
+apt install silversearcher-ag
 
 # Homeshick and configuration. Make sure this comes last!
 git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 source "$HOME/.homesick/repos/homeshick/homeshick.sh"
 homeshick clone git@github.com:Nnoerregaard/dotfiles.git
 homeshick link
+
+# Clone all active projects (corresponding to what you have in your tmuxinator
+mkdir ~/Documents/code && cd ~/Documents/code
+mkdir Houe && cd Houe && gh repo clone Nnoerregaard/MyTrash && npm install && cd .. # MyTrash
+mkdir Brandheroes && cd Brandheroes && # NB! This will fail until you can set up an SSH key for Bitbucket automatically or Brandheroes moves to GitHub)
+      git clone git@bitbucket.org:brandheroes/brandheroes-webapp-v2.git webapp/ && cd webapp && yarn && cd .. &&
+      git clone git@bitbucket.org:brandheroes/brandheroesapp.git mobile_app/ && cd mobile_app && yarn && cd .. && 
+      git clone git@bitbucket.org:brandheroes/brandheroes-backend-gql.git backend/ && cd backend && yarn && cd ..
 
 echo "Congratulations! Your new Debian based machine should now be set up and ready to go. Restart your terminal to start enjoying your new development environment"
